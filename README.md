978cc49a794144a31b2433f341df1b4ea9d58d3e
Carlos Villavicencio <cvillavicencio@stackbuilders.com>
2022-03-20 19:15:01 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Mini example of Flask ORM with SQLAlchemy
+
+![Actions Workflow](https://github.com/po5i/flask-mini-orm/workflows/Flask/badge.svg)
+
+This is a examle repository for [my article](https://dev.to/po5i/).
+
+## Setup
+
+Create and activate the virtual environment
+
+```bash
+virtualenv venv
+source venv/bin/activate
+```
+
+Run the server
+
+```bash
+python app.py
+```
+
+Run the static type check
+
+```bash
+mypy app.py
+```
+
+The server will be up on [http://localhost:5000](http://localhost:5000) and a database file named `db.sqlite3` is created.
+
+## How it works
+
+The toy API has four endpoints:
+
+Endpoint | Description
+-- | --
+`GET /` | Returns hello world
+`GET /notifications` | Returns all notifications
+`POST /notifications` | Adds a notification on the DB
+`GET /notifications/unread` | Returns all unread notifications
+
+If you want to add a notification on the DB, you can use this cURL command:
+
+```bash
+curl -X POST \
+  'http://localhost:5000/notifications' \
+  --header 'Accept: */*' \
+  --form 'description="Foo"' \
+  --form 'date="2022-03-20T18:09:22"' \
+  --form 'url="http://foo.bar.com"' \
+  --form 'email="foo@bar.com"'
+```
+
+## Requirements
+
+Python >= 3.9
+
+## License
+
+[MIT](http://www.opensource.org/licenses/mit-license.html)
---
+++ app.py
@@ -0,0 +1,80 @@
+from __future__ import annotations
+from datetime import datetime
+from typing import Optional, TypedDict
+
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from werkzeug import Response
+
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+db = SQLAlchemy(app)
+
+class NotificationsModel(TypedDict):
+    id: int
+    description: Optional[str]
+    email: str
+    date: datetime
+    url: Optional[str]
+    read: bool
+
+
+class Notifications(db.Model):  # type: ignore
+    __tablename__ = "notifications"
+
+    id = Column(Integer, primary_key=True, index=True)
+    description = Column(String(100))
+    email = Column(String(100), nullable=False)
+    date = Column(DateTime, nullable=False)
+    url = Column(String(200))
+    read = Column(Boolean, default=False)
+
+    def to_dict(self) -> NotificationsModel:
+        return {
+            "id": self.id,
+            "email": self.email,
+            "description": self.description,
+            "date": self.date,
+            "url": self.url,
+            "read": self.read,
+        }
+
+    @classmethod
+    def get_all(cls) -> list[Notifications]:
+        return db.session.query(cls).all()
+
+    @classmethod
+    def get_unread(cls) -> list[Notifications]:
+        return db.session.query(cls).filter(Notifications.read == False).all()
+
+
+@app.route("/")
+def index() -> Response:
+    return jsonify({"hello": "world"})
+
+
+@app.route("/notifications", methods=["GET", "POST"])
+def notifications() -> Response:
+    if request.method == "POST":
+        new_notification = Notifications(
+            **dict(request.form, date=datetime.fromisoformat(request.form["date"]))
+        )
+        db.session.add(new_notification)
+        db.session.commit()
+
+    notifications = Notifications.get_all()
+    return jsonify([notification.to_dict() for notification in notifications])
+
+
+@app.route("/notifications/unread")
+def unread() -> Response:
+    notifications = Notifications.get_unread()
+    return jsonify([notification.to_dict() for notification in notifications])
+
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
